b4040c1f38c48e4b9609f9ddb466840da0241716
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-04-14 13:02:14 +0530

    Add MongoDB connection and update environment configuration

---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+function connectToDb(){
+    mongoose.connect(process.env.DB_connect)
+        .then(() => {
+            console.log('Connected to MongoDB')
+        })
+        .catch(err => {
+            console.error('Error connecting to MongoDB', err)
+            
+        });
+}
+
+module.exports = connectToDb;
